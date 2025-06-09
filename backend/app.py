89a494c7fdc3544097676893d43e9f48188646bd
appsy-gc <167824281+appsy-gc@users.noreply.github.com>
2025-06-10 07:07:49 +1000

    Frontend dockerized

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:8080"])
 DATABASE = "db.sqlite3"
 
 def init_db():
